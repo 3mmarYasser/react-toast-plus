6e5ae28035b7ca6f97c5dabb5340b99928d2d367
3mmarYasser <Ammar.23120273@muc.edu.eg>
2024-09-19 22:46:33 +0300

    fix

--- src/utils/classNames.helper.ts
+++ src/utils/classNames.helper.ts
@@ -1,16 +1,6 @@
-type ClassValue = string | undefined | null | boolean | Record<string, boolean>;
-
-export const classNames = (...args: ClassValue[]): string => {
+export const classNames = (...args: Array<string | undefined | null | boolean>) => {
     return args
-        .flatMap(arg => {
-            if (typeof arg === 'string') {
-                return arg;
-            } else if (typeof arg === 'object' && arg !== null) {
-                return Object.keys(arg).filter(key => arg[key]);
-            }
-            return [];
-        })
         .filter(Boolean)
         .join(' ')
-        .trim();
+        .trim(); // Removes any extra spaces
 };
